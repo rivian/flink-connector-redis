d086934ca3f1eb11e786f2eb833c72f277177b85
Pramod Immaneni <pimmaneni@rivian.com>
2023-05-24 09:53:40 -0700

    Copyright notice

--- src/main/java/com/rivian/flink/connector/redis/RedisSubscriberSource.java
+++ src/main/java/com/rivian/flink/connector/redis/RedisSubscriberSource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Rivian Automotive, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rivian.flink.connector.redis;
 
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
@@ -5,9 +21,6 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 import redis.clients.jedis.*;
 
-/**
- * Created by Pramod Immaneni <pimmaneni@rivian.com> on 5/1/22
- */
 public class RedisSubscriberSource extends RichParallelSourceFunction<Message> {
 
     transient UnifiedJedis jedis;
--- src/test/java/com/rivian/flink/connector/redis/RedisSourceTest.java
+++ src/test/java/com/rivian/flink/connector/redis/RedisSourceTest.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright 2023 Rivian Automotive, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rivian.flink.connector.redis;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 
-/**
- * Created by Pramod Immaneni <pimmaneni@rivian.com> on 5/12/22
- */
 public class RedisSourceTest {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
